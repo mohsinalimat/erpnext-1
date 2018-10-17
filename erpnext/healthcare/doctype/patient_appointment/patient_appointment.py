c4446a0f07686198b321aa1d4a6379974819df97
Jamsheer <jamsheer@earthianslive.com>
2018-10-17 20:38:21 +0530

    fix: Patient Appointment - Calendar Color

--- erpnext/healthcare/doctype/patient_appointment/patient_appointment.py
+++ erpnext/healthcare/doctype/patient_appointment/patient_appointment.py
@@ -375,6 +375,7 @@ def get_events(start, end, filters=None):
 	"""
 	from frappe.desk.calendar import get_event_conditions
 	conditions = get_event_conditions("Patient Appointment", filters)
+
 	data = frappe.db.sql("""
 		select
 		`tabPatient Appointment`.name, `tabPatient Appointment`.patient,
