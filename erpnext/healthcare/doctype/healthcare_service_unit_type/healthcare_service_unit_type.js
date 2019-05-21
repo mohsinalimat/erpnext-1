d015420b3a52d6c0fc942491818004ce1b5a79f1
Jamsheer <jamsheer@earthianslive.com>
2019-05-21 14:34:05 +0530

    fix: Is Modality option in Healthcare Service Unit Type

--- erpnext/healthcare/doctype/healthcare_service_unit_type/healthcare_service_unit_type.js
+++ erpnext/healthcare/doctype/healthcare_service_unit_type/healthcare_service_unit_type.js
@@ -29,6 +29,7 @@ frappe.ui.form.on('Healthcare Service Unit Type', {
 					disable(frm, 1);
 				} );
 			}
+			frm.trigger("is_modality");
 		}
 	},
 	rate: function(frm) {
@@ -45,7 +46,29 @@ frappe.ui.form.on('Healthcare Service Unit Type', {
 		if(!frm.doc.__islocal){
 			frm.doc.change_in_item = 1;
 		}
-	}
+	},
+	is_modality: function(frm) {
+		if(frm.doc.is_modality == 1){
+			frm.set_value("overlap_appointments", false);
+			frm.set_value("allow_procedures", false);
+			frm.set_df_property("modality_type", "reqd", true);
+			frm.set_df_property("modality_name", "reqd", true);
+		}
+		else{
+			frm.set_df_property("modality_type", "reqd", false);
+			frm.set_df_property("modality_name", "reqd", false);
+		}
+	},
+	overlap_appointments: function(frm) {
+		if(frm.doc.overlap_appointments == 1){
+			frm.set_value("is_modality", false);
+		}
+	},
+	allow_procedures: function(frm) {
+		if(frm.doc.allow_procedures == 1){
+			frm.set_value("is_modality", false);
+		}
+	},
 });
 
 var disable = function(frm, status){
