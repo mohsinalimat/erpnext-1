e64b8b7b0e55f2b07c9903a797d74ac4beec810e
Jamsheer <jamsheer@earthianslive.com>
2019-05-21 18:06:32 +0530

    fix: Healthcare DocType - Item management

---
+++ erpnext/healthcare/healthcare_doc_item.js
@@ -0,0 +1,74 @@
+
+var item_action_buttons = function(frm) {
+	if(!frm.doc.__islocal) {
+		frm.add_custom_button(__('Change Item Code'), function() {
+			change_item_code(frm);
+		} );
+		if(frm.doc.disabled == 1){
+			frm.add_custom_button(__('Enable Template'), function() {
+				enable_disable_template(frm, 0);
+			} );
+		}
+		else{
+			frm.add_custom_button(__('Disable Template'), function() {
+				enable_disable_template(frm, 1);
+			} );
+		}
+	}
+}
+
+var enable_disable_template = function(frm, disabled){
+	frappe.call({
+		method: "erpnext.healthcare.utils.disable_enable_template",
+		args: {doctype: "Radiology Procedure", docname: frm.doc.name, 'item_code': frm.doc.item || frm.doc.item_code, disabled: disabled},
+		callback: function(){
+			frm.reload_doc();
+		}
+	});
+};
+
+var change_item_code = function(frm){
+	var doc = frm.doc;
+	var d = new frappe.ui.Dialog({
+		title:__("Change Item Code"),
+		fields:[
+			{
+				"fieldtype": "Data",
+				"label": "Item Code",
+				"fieldname": "item_code",
+				reqd:1
+			},
+			{
+				"fieldtype": "Button",
+				"label": __("Change Code"),
+				click: function() {
+					var values = d.get_values();
+					if(!values)
+						return;
+					change_item_code_from_doc(values["item_code"], doc);
+					d.hide();
+				}
+			}
+		]
+	});
+	d.show();
+	d.set_values({
+		'item_code': doc.item_code
+	});
+};
+
+var change_item_code_from_doc = function(item_code, doc){
+	frappe.call({
+		"method": "erpnext.healthcare.utils.change_item_code_from_doc",
+		"args": {item_code: item_code, doc: doc},
+		callback: function () {
+			cur_frm.reload_doc();
+			frappe.show_alert({
+				message: __("Item Code renamed successfully"),
+				indicator: "green"
+			});
+		},
+		freeze: true,
+		freeze_message: __("Changing Item Code...")
+	});
+};
--- erpnext/healthcare/utils.py
+++ erpnext/healthcare/utils.py
@@ -3,7 +3,7 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+import frappe, json
 import datetime
 from frappe import _
 import math
@@ -666,3 +666,92 @@ def get_patient_vitals(patient, from_date=None, to_date=None):
 		return vitals
 	else:
 		return False
+
+@frappe.whitelist()
+def disable_enable_template(doctype, docname, item_code, disabled):
+	frappe.db.set_value(doctype, docname, "disabled", disabled)
+	if (frappe.db.exists({ #set Item's disabled field to status
+		"doctype": "Item",
+		"item_code": item_code})):
+			frappe.db.set_value("Item", item_code, "disabled", disabled)
+	return
+
+@frappe.whitelist()
+def change_item_code_from_doc(item_code, doc):
+	args = json.loads(doc)
+	doc = frappe._dict(args)
+	if(frappe.db.exists({
+		"doctype": "Item",
+		"item_code": item_code})):
+		frappe.throw(_("Code {0} already exist").format(item_code))
+	else:
+		frappe.rename_doc("Item", doc.item_code, item_code, ignore_permissions = True)
+		frappe.db.set_value(doc.doctype, doc.name, "item_code", item_code)
+	return
+
+def create_item_from_doc(doc, item_name):
+	if(doc.is_billable == 1):
+		disabled = 0
+	else:
+		disabled = 1
+	#insert item
+	item =  frappe.get_doc({
+	"doctype": "Item",
+	"item_code": doc.item_code,
+	"item_name": item_name,
+	"item_group": doc.item_group,
+	"description": doc.description,
+	"is_sales_item": 1,
+	"is_service_item": 1,
+	"is_purchase_item": 0,
+	"is_stock_item": 0,
+	"show_in_website": 0,
+	"is_pro_applicable": 0,
+	"disabled": disabled,
+	"stock_uom": "Unit"
+	}).insert(ignore_permissions=True)
+
+	#insert item price
+	#get item price list to insert item price
+	if(doc.rate != 0.0):
+		price_list_name = frappe.db.get_value("Price List", {"selling": 1})
+		if(doc.rate):
+			make_item_price(item.name, price_list_name, doc.rate)
+		else:
+			make_item_price(item.name, price_list_name, 0.0)
+	#Set item to the template
+	frappe.db.set_value(doc.doctype, doc.name, "item", item.name)
+
+	doc.reload() #refresh the doc after insert.
+
+def make_item_price(item, price_list_name, item_price):
+	frappe.get_doc({
+		"doctype": "Item Price",
+		"price_list": price_list_name,
+		"item_code": item,
+		"price_list_rate": item_price
+	}).insert(ignore_permissions=True)
+
+def update_item_from_doc(doc, item_name):
+	if(doc.is_billable == 1 and doc.item):
+		updating_item(doc, item_name)
+		if(doc.rate != 0.0):
+			updating_rate(doc, item_name)
+	elif(doc.is_billable == 0 and doc.item):
+		frappe.db.set_value("Item", doc.item, "disabled", 1)
+
+def updating_item(doc, item_name):
+	frappe.db.sql("""update `tabItem` set item_name=%s, item_group=%s, disabled=0,
+		description=%s, modified=NOW() where item_code=%s""",
+		(item_name, doc.item_group , doc.description, doc.item))
+
+def updating_rate(doc, item_name):
+	frappe.db.sql("""update `tabItem Price` set item_name=%s, price_list_rate=%s, modified=NOW() where
+	 item_code=%s""",(item_name, doc.rate, doc.item))
+
+def on_trash_doc_having_item_reference(doc):
+	if(doc.item):
+		try:
+			frappe.delete_doc("Item",doc.item)
+		except Exception:
+			frappe.throw(_("Not permitted. Please disable the {0}").format(doc.doctype))
