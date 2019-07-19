cc51abdc3e5c5000b57b67d8b543a4bd272d2680
muhammad <mammuz77@gmail.com>
2019-07-19 15:40:35 +0530

    Lab Test - Create multiple - filter updated

--- erpnext/healthcare/doctype/lab_test/lab_test_list.js
+++ erpnext/healthcare/doctype/lab_test/lab_test_list.js
@@ -32,7 +32,7 @@ var create_multiple_dialog = function(listview){
 					return {
 						filters: {
 							"patient": dialog.get_value("patient"),
-							"docstatus": 1 ? dialog.get_value("doctype") == 'Sales Invoice' : ["<", 2]
+							"docstatus": ["<", 2]
 						}
 					};
 				}
