bf90bae7fb8b07ac64b5b928714ad765a2d7fcea
akash <akashkrishna619@gmail.com>
2019-07-26 17:43:25 +0530

    fix: Create Multiple clinical procedure - fix

--- erpnext/healthcare/doctype/clinical_procedure/clinical_procedure_list.js
+++ erpnext/healthcare/doctype/clinical_procedure/clinical_procedure_list.js
@@ -10,11 +10,11 @@ onload: function(listview) {
 
 var create_multiple_dialog = function(listview){
   var dialog = new frappe.ui.Dialog({
-		title: 'Create Multiple Lab Test',
+		title: 'Create Multiple Clinical Procedure',
 		width: 100,
 		fields: [
 			{fieldtype: "Link", label: "Patient", fieldname: "patient", options: "Patient", reqd: 1},
-			{fieldtype: "Link", fieldname: "docname", options: "Patient Encounter", reqd: 1,
+			{fieldtype: "Link", label: "Patient Encounter", fieldname: "patient_encounter", options: "Patient Encounter", reqd: 1,
 				get_query: function(){
 					return {
 						filters: {
@@ -25,23 +25,23 @@ var create_multiple_dialog = function(listview){
 				}
 			}
 		],
-    primary_action_label: __("Create Lab Test"),
+    primary_action_label: __("Create Clinical Procedure"),
 		primary_action : function(){
 			frappe.call({
 				method: 'erpnext.healthcare.doctype.clinical_procedure.clinical_procedure.create_multiple',
 				args:{
-					'docname': dialog.get_value("docname")
+					'docname': dialog.get_value("patient_encounter")
 				},
 				callback: function(data) {
 					if(!data.exc){
 						if(!data.message){
-							frappe.msgprint(__("No Lab Test created"))
+							frappe.msgprint(__("No Clinical Procedure created"))
 						}
 						listview.refresh();
 					}
 				},
 				freeze: true,
-				freeze_message: "Creating Lab Test..."
+				freeze_message: "Creating Clinical Procedure..."
 			});
 			dialog.hide();
 		}
