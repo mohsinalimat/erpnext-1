00472539cc48267b7650e73b5646e900c6f65810
Jamsheer <jamsheer@earthianslive.com>
2019-08-21 15:10:22 +0530

    fix: Patch - Clinical Procedure - standard_selling_rate type Currency

--- erpnext/patches/v12_0/reload_clinical_procedure_for_fieldtype.py
+++ erpnext/patches/v12_0/reload_clinical_procedure_for_fieldtype.py
@@ -2,4 +2,5 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
+	frappe.db.sql("alter table `tabClinical Procedure` change standard_selling_rate standard_selling_rate decimal(18,6) default '0.0'")
 	frappe.reload_doc("healthcare", "doctype", "clinical_procedure")
