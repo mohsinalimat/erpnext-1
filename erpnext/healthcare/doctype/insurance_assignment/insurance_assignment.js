5912af9c3528426f09b3fad7890bc1f9af05ef70
muhammad <mammuz77@gmail.com>
2019-09-04 14:31:33 +0530

    fix: Healthcare insurance- insurance company validation in assignment

--- erpnext/healthcare/doctype/insurance_assignment/insurance_assignment.js
+++ erpnext/healthcare/doctype/insurance_assignment/insurance_assignment.js
@@ -4,6 +4,30 @@
 frappe.ui.form.on('Insurance Assignment', {
 	refresh: function(frm) {
 	},
+	insurance_company: function(frm){
+		if(frm.doc.insurance_company){
+			frappe.call({
+				"method": "frappe.client.get_value",
+				args: {
+					doctype: "Insurance Contract",
+					filters: {
+						'insurance_company': frm.doc.insurance_company,
+						'is_active':1,
+						'end_date':[">=", frappe.datetime.nowdate()],
+						'docstatus':1
+					},
+					fieldname: ['name']
+				},
+				callback: function (data) {
+					if(!data.message){
+						frappe.msgprint(__("There is no valid contract with this Insurance Company {0}",[frm.doc.insurance_company]));
+						frm.set_value("insurance_company", "");
+						frm.set_value("insurance_company_name", "");
+					}
+				}
+			});
+		}
+	},
 	patient: function(frm){
 		if(frm.doc.patient){
 			frappe.call({
