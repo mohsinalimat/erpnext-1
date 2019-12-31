c363dced910092d0c3afc22423d3a2bec45509c8
akash <akashkrishna619@gmail.com>
2019-12-31 13:03:41 +0530

    fix: Appointment Type - medical code and standard

--- erpnext/healthcare/doctype/appointment_type/appointment_type.js
+++ erpnext/healthcare/doctype/appointment_type/appointment_type.js
@@ -2,4 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Appointment Type', {
+	onload:function(frm){
+		frm.set_query('medical_code', () => {
+    return {
+        filters: {
+            medical_code_standard: frm.doc.medical_code_standard
+        }
+    }
+})
+	},
 });
