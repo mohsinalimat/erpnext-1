26838be6a4c0e613daf6870856d1140ee8d22a42
muhammad <mammuz77@gmail.com>
2020-01-17 12:54:02 +0530

    fix:set default start date and end date

--- erpnext/healthcare/doctype/insurance_contract/insurance_contract.js
+++ erpnext/healthcare/doctype/insurance_contract/insurance_contract.js
@@ -3,6 +3,19 @@
 
 frappe.ui.form.on('Insurance Contract', {
 	refresh: function(frm) {
-
+		frm.set_query("price_list", function() {
+			return {
+				filters: {
+					'selling':1
+				}
+			};
+		});
+		if(frm.is_new){
+			frm.set_value("start_date", frappe.datetime.get_today())
+		}
+	},
+	start_date: function(frm){
+		var to_date=frappe.datetime.add_days(frm.doc.start_date, 365)
+		frm.set_value("end_date", to_date);
 	}
 });
